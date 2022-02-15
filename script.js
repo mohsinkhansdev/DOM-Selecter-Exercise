4b30d492e6577765242c5f1f00e2babe0e752818
Muhammad Mohsin Khan <77793691+mohsinkhansdev@users.noreply.github.com>
2022-02-15 23:19:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var h11 = document.getElementsByTagName("h1");
+h11
+
+var pid = document.getElementById("first");
+pid
+
+var pclass = document.getElementsByClassName("special");
+pclass
+
+var pclass = document.getElementsByClassName("special")[0];
+pclass
+
+var lclass = document.getElementsByClassName("last");
+lclass
+
+document.querySelector("#first");
+<p id=​"first" class=​"special">​Hello​</p>
+
+document.querySelectorAll(".special");
+
+document.querySelectorAll(".special") [0];
+<p id=​"first" class=​"special">​Hello​</p>​
+
+document.querySelector(".special");
+
+document.querySelector("h1 + p");
+<p id=​"first" class=​"special">​Hello​</p>​
